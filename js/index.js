d63c505f74ec4a1d118ea25db94bc8914e124dc6
shivam-dsk <shivam.sharma@dishakiran.com>
2023-03-16 23:40:28 +0530

    added projecr section

--- js/index.js
+++ js/index.js
@@ -1,22 +1,31 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+function openImage(img){
+    document.getElementById("big-certificate").src = img;
+}
 
-const app = express()
-app.use(bodyParser.urlencoded({extended : true}))
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname+"/home.html")
-})
 
-// app.post("/", function(req, res){
-//     console.log(req.body)
-//     var num1 = Number(req.body.num1);
-//     var num2 = Number(req.body.num2);
-//     res.send("result is "+ (num1+num2))
+
+
+
+// const express = require("express")
+// const bodyParser = require("body-parser")
+
+// const app = express()
+// app.use(bodyParser.urlencoded({extended : true}))
+
+// app.get("/", function(req, res){
+//     res.sendFile(__dirname+"/home.html")
 // })
 
-// app.get("/result", function(req, res){
-//     res.send("<h1>This is result page</h1>")
-// })
+// // app.post("/", function(req, res){
+// //     console.log(req.body)
+// //     var num1 = Number(req.body.num1);
+// //     var num2 = Number(req.body.num2);
+// //     res.send("result is "+ (num1+num2))
+// // })
 
-app.listen(3000)
+// // app.get("/result", function(req, res){
+// //     res.send("<h1>This is result page</h1>")
+// // })
+
+// app.listen(3000)
